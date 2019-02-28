a07b9a186493975e3c62df4a1322b90c5a5f850b
Cathy <katpie613@gmail.com>
2019-02-28 13:25:35 -0500

    random solution

--- src/main/java/domain/SlideShow.java
+++ src/main/java/domain/SlideShow.java
@@ -18,12 +18,24 @@ public class SlideShow {
 		currentSlides.add(slide);	
 		photos.remove(randomInitialPhoto);
 		
-		for(Photo photo : photos) {
+		
+		for(Photo photo : photos) {		
+			Slide newSlide = new Slide();
+			int photoIndex = random.nextInt(photos.size()-1);
+			
 			// take 2 pictures
-			if(random.nextBoolean()) {
-				
+			if(photos.get(photoIndex).vertical) {
+				newSlide.photos.add(photos.get(photoIndex))
+				//get another vertical photo
+				for (Photo vertPhoto : photos)
+					int nextPhoto = random.nextInt(photos.size()-1);
+					if(photos.get(nextPhoto).vertical) {
+						slide.photos.add()
+					
+					}
 			// take 1 picture
 			} else {
+				newSlide.photos.add(photos.get(photoIndex))
 				
 			}
 		}
