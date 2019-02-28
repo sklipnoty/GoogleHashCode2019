a46a221978eb4de648dd10dd638993d8321e1121
Stephan Bouwer <47927140+stephanfbouwer@users.noreply.github.com>
2019-02-28 19:43:14 +0100

    calculate intrest
    Calculate Intrest

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -1,7 +1,9 @@
 import java.util.List;
 
 import domain.Photo;
+import domain.Slide;
 import io.InputReader;
+import utils.CalculateIntrest;
 
 public class StartUp {
 	public static void main(String args[]) {
@@ -11,5 +13,53 @@ public class StartUp {
 		for(Photo photo : photos) {
 			System.out.println(photo);
 		}
+		
+		
+		// Calculate intrest testing
+		/*
+		Slide a = new Slide();
+		Photo aa = new Photo();
+		aa.tags.add("aaa");
+		aa.tags.add("bbb");
+		aa.tags.add("ccc");
+		a.photos.add(aa );
+		
+		Slide b = new Slide();
+		Photo bb = new Photo();
+		bb.tags.add("ccc");
+		bb.tags.add("dd");
+		bb.tags.add("ee");
+		b.photos.add(bb);
+		
+		int intrest = CalculateIntrest.CalculateIntrest(a, b);
+		
+		System.out.println( " intrest: " +  intrest);
+		*/
+		
+		
+	// Calculate intrest testing
+		
+		Slide a = new Slide();
+		Photo aa = new Photo();
+		aa.tags.add("aaa");
+		aa.tags.add("bbb");
+		aa.tags.add("eee");
+		aa.tags.add("bbb");
+		aa.tags.add("ccc");
+		a.photos.add(aa );
+		
+		Slide b = new Slide();
+		Photo bb = new Photo();
+		bb.tags.add("ccc");
+		bb.tags.add("bbb");
+		bb.tags.add("eee");
+		bb.tags.add("bbb");
+		bb.tags.add("ee");
+		b.photos.add(bb);
+		
+		int intrest = CalculateIntrest.CalculateIntrest(a, b);
+		
+		System.out.println( " intrest: " +  intrest);
+		
 	}
 }
---
+++ src/main/java/utils/CalculateIntrest.java
@@ -0,0 +1,115 @@
+package utils;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import domain.Photo;
+import domain.Slide;
+
+public class CalculateIntrest {
+	
+	
+	// Slide
+	public static int CalculateIntrest(Slide currentPhotoSlide, Slide nextPhotoSlide)
+	{
+		
+		
+		List<Photo> currentPhotoList = currentPhotoSlide.photos;
+		Set<String> currentSlideTags = new HashSet<String>();
+		
+		// first slide
+		for (Photo photo : currentPhotoList) 
+		{
+			for (String string : photo.tags) 
+			{
+				currentSlideTags.add(string);
+			}
+			
+		}
+		
+		Photo currentPhoto = new Photo();
+		currentPhoto.tags. addAll(currentSlideTags);
+		
+		
+		// second slide
+		
+
+		List<Photo> nextPhotoList = nextPhotoSlide.photos;
+		Set<String> nextSlideTags = new HashSet<String>();
+		
+		for (Photo photo : nextPhotoList) 
+		{
+			for (String string : photo.tags) 
+			{
+				nextSlideTags.add(string);
+			}
+			
+		}
+		
+		Photo nextPhoto = new Photo();
+		nextPhoto.tags. addAll(nextSlideTags);
+		
+		int numberOfCommonTags = 0;
+		int numberOfTagsInCurrentButNotNext = 0;
+		int numberOfTagsInNexbutNotCurrent = 0;
+		
+		
+		List<String> currentPhotosTags = currentPhoto.tags;
+		List<String> nextPhotoTags = nextPhoto.tags;
+		
+		// numberOfTagsInCurrentButNotNext = currentPhotosTags.size();
+		
+		
+		// common tags
+		for (String currentTag : currentPhotosTags) 
+		{
+			for (String nextPhotoTag : nextPhotoTags) 
+			{
+				if(currentTag.equals(nextPhotoTag))
+				{
+					++numberOfCommonTags;
+				}
+			}
+			
+		}
+		
+		
+		numberOfTagsInCurrentButNotNext = currentPhoto.tags.size();
+		for (String nextTag : nextPhotoTags) 
+		{
+			for (String currentTag : currentPhotosTags) 
+			{
+				if(nextTag.equals(currentTag))
+				{
+					--numberOfTagsInCurrentButNotNext;
+				}
+			}
+		}
+		
+		
+		numberOfTagsInNexbutNotCurrent = nextPhoto.tags.size();
+		for (String currentTag : currentPhotosTags) 
+		{
+			for (String nextTag : nextPhotoTags) 
+			{
+				if(currentTag.equals(nextTag))
+				{
+					--numberOfTagsInNexbutNotCurrent;
+				}
+			}
+		}
+		
+		
+		System.out.println("IntrestCheck");
+		System.out.println("numberOfCommonTags : " +numberOfCommonTags);
+		System.out.println("numberOfTagsInCurrentButNotNext : " +numberOfTagsInCurrentButNotNext);
+		System.out.println("numberOfTagsInNexbutNotCurrent : " +numberOfTagsInNexbutNotCurrent);
+		System.out.println("Result : " +Math.min(numberOfCommonTags, Math.min(numberOfTagsInCurrentButNotNext,numberOfTagsInNexbutNotCurrent)) );
+		
+		
+		return Math.min(numberOfCommonTags, Math.min(numberOfTagsInCurrentButNotNext,numberOfTagsInNexbutNotCurrent));
+		
+	}
+
+}
